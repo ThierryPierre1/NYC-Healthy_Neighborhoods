3ecf463fc3f3d3919799bbd3bd6294e19080b69e
Tpierre31 <thierry.pierre@stonybrook.edu>
2024-01-08 21:04:17 -0500

    commit

--- Data Processing.py
+++ Data Processing.py
@@ -5,10 +5,11 @@ import numpy as np
 ### Combine all the data into one file
 
 # Read file path
-File_path = r'/Users/thierrypierre/Downloads/CCC Data Download_20231206_002841610'
+File_path = "/Users/thierrypierre/Documents/GitHub/NYC-Healthy_Neighborhoods/Data/NYC-Health_Homes files"
 
 # Create a list of all the files in the folder
 File_list = os.listdir(File_path)
+print(File_list)
 
 # Create a list of all the files that are csv files
 File_list_csv = [file for file in File_list if file.endswith('.csv')]
